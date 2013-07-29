95bb2fa206c302ed20bca7a990ec5581b84f5daf
emmylifeline <emmylifeline@gmail.com>
2013-07-29 10:00:10 -0400

    implemented vclass function

--- 1nnr.ml
+++ 1nnr.ml
@@ -1,2 +1,21 @@
-let length = funtion(lst)-> List.length(lst);;
+let length = function(lst)-> List.length lst;;
 
+let square = function(x1,x2)-> (x1 -. x2) *. (x1 -. x2);;
+
+let distance = function(vect, vect2) ->();;
+	(*if(vect=vect2) then 
+		sqrt ()
+	else failwith "the two vectors must have the same size"*)
+
+
+let distanceAllVectors2 = function(v, vset)->();;
+
+let t1NNR = function(distlist)-> ();;
+
+let vclass = function(v)-> List.nth v (length(v)-1);;
+
+let closest = function((min, loc),trset) -> ();;
+
+let closest_class = function((min, loc),trset) -> ();;
+
+let classNNR = function(x, ts)->();;
